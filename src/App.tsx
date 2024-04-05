00d1ea6aa2f8d373b517a1efbef4c38dfaad7ab5
Robin <Robin_Nygren@outlook.com>
2024-04-05 14:27:11 +0200

    sidebar with mouseover

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import "./app.scss";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
-  return <></>;
+  const items = ["Home", "About", "Services", "Contact"];
+  return (
+    <>
+      <Sidebar items={items} />
+    </>
+  );
 }
 
 export default App;
