5068059f2f4f592ca57969ae43499cfd5bca79af
biggieliten <filipisaksson00@gmail.com>
2024-04-08 14:26:42 +0200

    fixed css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import "./app.scss";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Toast from "./Components/Toast/Toast";
+import RadioButtons from "./Components/RadioButtonGroup/RadioButtons/RadioButtons";
 
 function App() {
   const items = ["Home", "About", "Services", "Contact"];
+  const RadioButtonsArray = [
+    { name: "Option 1" },
+    { name: "Option 2" },
+    { name: "Option 3" },
+    { name: "Option 4" },
+  ];
   return (
     <>
       <Sidebar items={items} />
       <Toast message="Error" type="error" />
       <Toast message="Success" type="success" />
       <Toast message="Info" type="info" />
+      <RadioButtons
+        FormHeader="Radio Group"
+        optionsArray={RadioButtonsArray}
+      />{" "}
     </>
   );
 }
